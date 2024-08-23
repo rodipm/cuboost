95e824c57cec553c6ecf85b9c5dee7e907b34489
Rodrigo Macharelli <rodrigo.macharelli@opus-software.com.br>
2024-08-23 17:57:59 -0300

    Updates

---
+++ assets/inside-KOB7JHHP-BXFWbMR8.js
@@ -0,0 +1,1 @@
+import{e as o}from"./search-worker-entry-BzV0m-l-.js";import{w as e}from"./index-1TffcFJN.js";o(e);
--- assets/inside-KOB7JHHP-CLmP8UqF.js
+++
@@ -1,1 +0,0 @@
-import{e as o}from"./search-worker-entry-DjJjpZf-.js";import{w as e}from"./index-geJ5E9sf.js";o(e);
